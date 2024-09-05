f232027efc56d98db52c47a324f548446ffe2196
mohit0224 <mohitdheer24+git@gmail.com>
2024-09-05 18:18:57 +0530

    update

--- src/middlewares/isHttps.middleware.js
+++ src/middlewares/isHttps.middleware.js
@@ -1,15 +1,14 @@
 import { isProduction } from "../app.js";
 
 const isHttps = (req, res, next) => {
-    console.log(isProduction);
-
     if (isProduction) {
         if (req.secure) {
+            console.log(req.secure);
             return next();
         }
         res.redirect(`https://${req.headers.host}${req.url}`);
     }
-    next();
+    return next();
 };
 
 export default isHttps;
--- src/utils/asynchandler.utils.js
+++ src/utils/asynchandler.utils.js
@@ -1,6 +1,10 @@
 const asyncHandler = (func) => {
     return (req, res, next) => {
-        Promise.resolve(func(req, res, next)).catch((err) => next(err));
+        Promise.resolve(func(req, res, next)).catch((err) => {
+            if (!res.headersSent) {
+                next(err);
+            }
+        });
     };
 };
 
