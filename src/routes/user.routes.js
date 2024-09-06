58e9a413e64cdcf394aa85f2cdcdf2e1ad51f141
mohit0224 <mohitdheer24+git@gmail.com>
2024-09-06 13:43:34 +0530

    user router - add rate limiter in signup

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { createAccount, getUser } from "../controllers/user.controllers.js";
 import isLoggedIn from "../middlewares/isLoggedIn.middleware.js";
+import limiter from "../middlewares/rateLimiter.middleware.js";
 
 const router = Router();
 
-router.post("/", createAccount);
+router.post("/", limiter, createAccount);
 
 router.get("/", isLoggedIn, getUser);
 
