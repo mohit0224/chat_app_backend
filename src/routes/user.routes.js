2a693417c3e5c55109c8b8e4a14e1b1564309f20
mohit0224 <mohitdheer24+git@gmail.com>
2024-09-06 12:32:21 +0530

    user.routes - get => post

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,7 +4,7 @@ import isLoggedIn from "../middlewares/isLoggedIn.middleware.js";
 
 const router = Router();
 
-router.get("/create", createAccount);
+router.post("/create", createAccount);
 
 router.get("/", isLoggedIn, getUser);
 
